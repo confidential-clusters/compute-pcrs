5b0875219a7e0d472e11fdf5a60849cdaf94ed12
Timothée Ravier <tim@siosm.fr>
2025-06-15 11:29:34 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -88,60 +88,47 @@ fn compute_pcr11() {
 
     let uki = "a9ea995b29cda6783b676e2ec2666d8813882b604625389428ece4eee074e742.efi";
     let pe: lief::pe::Binary = lief::pe::Binary::parse(&format!("./test/{}", uki)).unwrap();
-    let hashes = sections.iter().for_each(|s| {
+    let mut hashes: Vec<(String, Vec<u8>)> = vec![];
+    sections.iter().for_each(|s| {
         let section = pe.section_by_name(s).unwrap();
-        println!("{}", hex::encode(Sha256::digest(format!("{}\0", s)).to_vec()));
-        println!("{}", hex::encode(Sha256::digest(section.content()).to_vec()));
+        hashes.push(
+            (
+                (*s).into(),
+                Sha256::digest(format!("{}\0", s)).to_vec(),
+            )
+        );
+        hashes.push(
+            (
+                (*s).into(),
+                Sha256::digest(section.content()).to_vec(),
+            )
+        );
     });
 
-    // let ev_efi_action_hash: Vec<u8> = Sha256::digest(sections[0]).to_vec();
-    // // let ev_separator_hash: Vec<u8> = Sha256::digest(hex::decode("00000000").unwrap()).to_vec();
+    // Start with 0
+    let mut result = hex::decode("0000000000000000000000000000000000000000000000000000000000000000").unwrap().to_vec();
 
-    // let bins = vec![
-    //     "./test/shim.efi",
-    //     "./test/grubx64.efi",
-    //     "./test/a9ea995b29cda6783b676e2ec2666d8813882b604625389428ece4eee074e742.efi",
-    // ];
+    for (_s, h) in &hashes{
+        let mut hasher = Sha256::new();
+        hasher.update(result);
+        hasher.update(h);
+        result = hasher.finalize().to_vec();
+    }
 
-    // let mut hashes: Vec<Vec<u8>> = vec![];
-    // hashes.push(ev_efi_action_hash);
-    // hashes.push(ev_separator_hash);
+    // println!("{}", hex::encode(&result));
 
-    // let mut bin_hashes: Vec<Vec<u8>> = bins.iter().map(|b| {
-    //     let pe: lief::pe::Binary = lief::pe::Binary::parse(b).unwrap();
-    //     pe.authentihash(lief::pe::Algorithms::SHA_256)
-    // }).collect();
-    // hashes.append(&mut bin_hashes);
+    let pcr11 = PCR{
+        id: 11,
+        value:  hex::encode(result),
+        parts: hashes.iter().map(|(s, h)| {
+            Part {
+                name: s.into(),
+                hash: hex::encode(h),
+        }
+        }).collect(),
+    };
 
-    // // println!("{:?}", ev_efi_action_hash);
-    // // println!("{:?}", ev_separator_hash);
-    // // hashes.iter().for_each(|h| {
-    // //     println!("{:?}", h);
-    // // });
-
-    // // Start with 0
-    // let mut result = hex::decode("0000000000000000000000000000000000000000000000000000000000000000").unwrap().to_vec();
-
-    // for h in &hashes{
-    //     let mut hasher = Sha256::new();
-    //     hasher.update(result);
-    //     hasher.update(h);
-    //     result = hasher.finalize().to_vec();
-    // }
-
-    // // println!("{}", hex::encode(result));
-
-    // assert_eq!(result[..], hex!("1714C36BF81EF84ECB056D6BA815DCC8CA889074AFB69D621F1C4D8FA03B23F0"));
-
-    // let pcr4 = PCR{
-    //     id: 4,
-    //     value:  hex::encode(result),
-    //     parts: hashes.iter().map(|h| {
-    //         hex::encode(h)
-    //     }).collect(),
-    // };
-
-    // println!("{}", serde_json::to_string_pretty(&pcr4).unwrap());
+    println!("{}", serde_json::to_string_pretty(&pcr11).unwrap());
 }
 
 fn main() -> Result<()> {
