1c310e5a26beae377c048adc3ab3bc01b550ea41
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2025-07-10 17:18:12 +0200

    shim: Extend section names to their long representation
    Some section names are shortened up in some cases. The names of those
    sections is read instead by a string formatted such as "/<integer>". The
    <integer> is the offset of the strings table in which the long name of
    the section is stored. The strings table is right after the symbols
    table, so we can figure out where it starts knowing the beginning of the
    symbols table, the numer of symbols and that each symbol is 18 bytes.
    
    Finally, we read the long section name from the given offset of the
    file, until we encounter a null character.
    
    This extends the Shim struct to also contain the path to the shim binary
    file. That way, we can read at specific offsets to extract long section
    names.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

--- src/shim.rs
+++ src/shim.rs
@@ -1,8 +1,11 @@
 use crate::uefi::{GUID_SHIM_LOCK, UEFIVariableData};
 use lief::generic::Section;
+use std::fs::File;
+use std::os::unix::fs::FileExt;
 
 pub struct Shim {
     image: lief::pe::Binary,
+    path: String,
 }
 
 const SHIM_SBATLEVEL_SECTION: &str = ".sbatlevel";
@@ -12,12 +15,44 @@ impl Shim {
     pub fn load_from_file(path: &str) -> Shim {
         Shim {
             image: lief::pe::Binary::parse(path).unwrap(),
+            path: path.into(),
         }
+    }
+
+    pub fn image(&self) -> &lief::pe::Binary {
+        &self.image
+    }
+
+    fn long_section_name(&self, mut name: String) -> String {
+        if name.as_bytes()[0] as char != '/' {
+            return name;
+        }
+        name.remove(0);
+        // Symbols are 18 bytes long
+        let mut string_offset = self.image.header().pointerto_symbol_table()
+            + self.image.header().numberof_symbols() * 18
+            + name.parse::<u32>().unwrap();
+        let mut long_name = String::new();
+        let mut next_char: [u8; 1] = [0];
+        loop {
+            let image_file = File::open(&self.path).unwrap();
+            if image_file
+                .read_exact_at(&mut next_char, string_offset as u64)
+                .is_err()
+                || next_char[0] == 0
+            {
+                break;
+            }
+
+            long_name.push(next_char[0] as char);
+            string_offset += 1;
+        }
+        long_name
     }
 
     pub fn get_sbatlevel_uefivar(&self) -> Option<UEFIVariableData> {
         for section in self.image.sections() {
-            if section.name() == *SHIM_SBATLEVEL_SECTION {
+            if self.long_section_name(section.name()) == *SHIM_SBATLEVEL_SECTION {
                 return Some(UEFIVariableData::new(
                     GUID_SHIM_LOCK,
                     "SbatLevel",
