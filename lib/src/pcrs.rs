f88e9bca88d3ade28986a82f40535121e713b195
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2025-10-28 17:05:07 +0100

    pcrs: Cast TPMEvents into Part and Pcr structs
    Implement the from TPMEvent traits for Part and Pcr.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

--- lib/src/pcrs.rs
+++ lib/src/pcrs.rs
@@ -3,8 +3,14 @@
 //
 // SPDX-License-Identifier: MIT
 
+use crate::tpmevents::TPMEvent;
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
+use sha2::{Digest, Sha256};
+
+const PCR_INIT_VALUE: [u8; 32] = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
 
 #[cfg(test)]
 mod tests;
@@ -26,4 +32,32 @@ pub struct Pcr {
     #[serde_as(as = "serde_with::hex::Hex")]
     pub value: Vec<u8>,
     pub parts: Vec<Part>,
+}
+
+impl From<&TPMEvent> for Part {
+    fn from(event: &TPMEvent) -> Part {
+        Part {
+            name: event.name.clone(),
+            hash: event.hash.clone(),
+        }
+    }
+}
+
+impl Pcr {
+    pub fn compile_from(events: &Vec<TPMEvent>) -> Pcr {
+        let mut result = PCR_INIT_VALUE.to_vec();
+
+        for event in events {
+            let mut hasher = Sha256::new();
+            hasher.update(result);
+            hasher.update(event.hash.clone());
+            result = hasher.finalize().to_vec();
+        }
+
+        Pcr {
+            id: events[0].pcr.into(),
+            value: result,
+            parts: events.iter().map(|e| e.into()).collect(),
+        }
+    }
 }
